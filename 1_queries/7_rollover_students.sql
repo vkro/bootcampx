1a3157acf61fb91c81c0c8c7bb80a06900e1f418
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 01:26:40 +0000

    get all rollover students (started in one cohort, finished in another

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,4 @@
+SELECT  students.name AS student_name, cohorts.name AS cohort_name, cohorts.start_date AS cohort_start_date, students.start_date as student_start_date 
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.start_date <> students.start_date
+ORDER BY cohort_name;
