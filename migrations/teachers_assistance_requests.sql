84e872f84d1c5f35b2bd2fa3127bd0f4990bcb76
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 21:13:15 +0000

    edit teachers and assistance_request create table statememts

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,9 +1,9 @@
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
+  name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE,
-  is_active BOOLEAN
+  is_active BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE assistance_requests (
@@ -11,9 +11,9 @@ CREATE TABLE assistance_requests (
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
-  created_at DATETIME,
-  started_at DATETIME,
-  completed_at DATETIME,
-  student_feedback VARCHAR(255),
-  teacher_feedback VARCHAR(255)
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
 )
