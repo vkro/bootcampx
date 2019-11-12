880a5f6087b61d6ea9a9a16efa5ba45ee22a2ef2
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 23:57:45 +0000

    get names of teachers who performed assistance requests during a cohort

---
+++ 4_queries/12_assisting_teachers_by_cohort.sql
@@ -0,0 +1,10 @@
+-- Get names of teachers who performed assistance request during a cohort
+
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teachers.name;
+
