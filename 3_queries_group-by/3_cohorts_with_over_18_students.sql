2502675a9ed774ca5ea53dcc557fda7c9c7bbfdf
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 02:50:12 +0000

    get all cohorts wtih 18 or more students

---
+++ 3_queries_group-by/3_cohorts_with_over_18_students.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name AS cohort_name, COUNT(students.*) AS student_count
+FROM students JOIN cohorts ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING COUNT(students.*) >=18
+ORDER BY student_count;
