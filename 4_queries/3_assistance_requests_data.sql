41e61dc636cc220fb70a8db901bb2024c86eeb60
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 22:23:38 +0000

    get important data about each assistance request

---
+++ 4_queries/3_assistance_requests_data.sql
@@ -0,0 +1,6 @@
+SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, (assistance_requests.completed_at - assistance_requests.started_at) AS duration
+FROM assistance_requests
+JOIN assignments ON assignment_id = assignments.id
+JOIN teachers ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+ORDER BY duration;
