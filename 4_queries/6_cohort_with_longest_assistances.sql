b803af86e2f703d8d6e9b6d44c1bdfafc5c28a66
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 23:10:03 +0000

    get cohort with longest average duration of assistance requests

---
+++ 4_queries/6_cohort_with_longest_assistances.sql
@@ -0,0 +1,24 @@
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
+
+
+-- This also worked;
+-- SELECT cohorts.name, AVG(completed_at - started_at) AS average_assistance_time
+-- FROM cohorts
+-- JOIN students ON students.cohort_id = cohorts.id
+-- JOIN assistance_requests ON  students.id = student_id
+-- GROUP BY cohorts.id
+-- HAVING AVG(completed_at - started_at) >= ALL
+--     (SELECT AVG(completed_at - started_at)
+--       FROM cohorts
+--       JOIN students ON students.cohort_id = cohorts.id
+--       JOIN assistance_requests ON students.id = student_id
+--       GROUP BY cohorts.id
+--     )
+-- ORDER BY average_assistance_time DESC
+-- ;
