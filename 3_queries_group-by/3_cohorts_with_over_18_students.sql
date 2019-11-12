46da2f73b9229fbd426702cfb4cf340fbc625ce9
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-12 22:06:38 +0000

    formatting

--- 3_queries_group-by/3_cohorts_with_over_18_students.sql
+++ 3_queries_group-by/3_cohorts_with_over_18_students.sql
@@ -1,5 +1,6 @@
 SELECT cohorts.name AS cohort_name, COUNT(students.*) AS student_count
-FROM students JOIN cohorts ON students.cohort_id = cohorts.id
+FROM students 
+JOIN cohorts ON students.cohort_id = cohorts.id
 GROUP BY cohorts.name
 HAVING COUNT(students.*) >=18
 ORDER BY student_count;
