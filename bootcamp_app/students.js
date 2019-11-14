4e17755e385fc5d6d59edb1b317be78a883de929
Vanessa Kroeker <vkroeker@gmail.com>
2019-11-14 04:44:34 +0000

    parameterized student & teacher.js queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,24 +1,31 @@
 const { Pool } = require('pg');
 const userInput = process.argv.splice(2);
 
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = userInput[0];
+const limit = userInput[1];
+
+const values = [`%${cohortName}%`, limit];
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
-})
+});
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${userInput[0]}%'
-LIMIT ${userInput[1]};
-`)
+pool.query(queryString, values)
 
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort.`)
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`)
   })
 })
 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 const userInput = process.argv.splice(2);
 
-
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -9,15 +8,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT cohorts.name as cohort, teachers.name as teacher
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${userInput[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+const cohortName = userInput[0];
+const values = [`${cohortName}`]
+
+pool.query(queryString, values)
 
 .then(res => {
   res.rows.forEach(user => {
